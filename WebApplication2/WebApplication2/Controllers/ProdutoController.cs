329a5cccd9861bf82c4bc207502a02d32ed8282f
LINDEMBERG NUNES CAVALCANTE JUNIOR <lindemberg.j@academico.ifrn.edu.br>
2022-08-04 10:30:45 -0300

    crate e edit

--- WebApplication2/WebApplication2/Controllers/ProdutoController.cs
+++ WebApplication2/WebApplication2/Controllers/ProdutoController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 using WebApplication2.Models;
+using System.Net;
 
 namespace WebApplication2.Controllers
 {
@@ -13,7 +15,69 @@ namespace WebApplication2.Controllers
         // GET: Produtos
         public ActionResult Index()
         {
-            return View(context.Produtos.OrderBy(c => c.Nome));
+            var produtos = context.Produtos.Include(c => c.Categoria).Include(f => f.Fabricante). OrderBy(n => n.Nome);
+            return View(produtos);
+        }
+        // GET: Produtos/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoriaId = new SelectList(context.Categorias.OrderBy(b => b.Nome),
+            "CategoriaId", "Nome");
+            ViewBag.FabricanteId = new SelectList(context.Fabricantes.OrderBy(b => b.Nome),
+            "FabricanteId", "Nome");
+            return View();
+        }
+        // POST: Produtos/Create
+        [HttpPost]
+        public ActionResult Create(Produto produto)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                context.Produtos.Add(produto);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(produto);
+            }
+        }
+        // GET: Produtos/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = context.Produtos.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoriaId = new SelectList(context.Categorias.OrderBy(b => b.Nome), "CategoriaId",
+            "Nome", produto.CategoriaId);
+            ViewBag.FabricanteId = new SelectList(context.Fabricantes.OrderBy(b => b.Nome), "FabricanteId",
+            "Nome", produto.FabricanteId);
+            return View(produto);
+        }
+        [HttpPost]
+        public ActionResult Edit(Produto produto)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    context.Entry(produto).State = EntityState.Modified;
+                    context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(produto);
+            }
+            catch
+            {
+                return View(produto);
+            }
         }
     }
 }
