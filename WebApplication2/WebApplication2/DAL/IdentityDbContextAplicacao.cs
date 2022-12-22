bae54877cef01418b8774ee6c70565cf28b1f3d4
LINDEMBERG NUNES CAVALCANTE JUNIOR <lindemberg.j@academico.ifrn.edu.br>
2022-12-22 11:45:21 -0300

    usuario

---
+++ WebApplication2/WebApplication2/DAL/IdentityDbContextAplicacao.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using WebApplication2.Areas.Seguranca.Models;
+using System.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.DAL
+{
+    public class IdentityDbContextAplicacao : IdentityDbContext<Usuario>
+    {
+        public IdentityDbContextAplicacao() : base("IdentityDb")
+        { }
+        static IdentityDbContextAplicacao()
+        {
+            Database.SetInitializer<IdentityDbContextAplicacao>(new IdentityDbInit());
+        }
+        public static IdentityDbContextAplicacao Create()
+        {
+            return new IdentityDbContextAplicacao();
+        }
+        public class IdentityDbInit :DropCreateDatabaseIfModelChanges<IdentityDbContextAplicacao>
+        {
+
+        }
+    }
+}
+
