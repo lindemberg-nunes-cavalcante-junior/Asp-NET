36d2872e1200b31ebcb607f99a4312f246947145
LINDEMBERG NUNES CAVALCANTE JUNIOR <lindemberg.j@academico.ifrn.edu.br>
2023-01-16 10:23:47 -0300

    edição de usuario feita

--- WebApplication2/WebApplication2/Areas/Seguranca/Controllers/AdminController.cs
+++ WebApplication2/WebApplication2/Areas/Seguranca/Controllers/AdminController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNet.Identity.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication2.Areas.Seguranca.Models;
 using WebApplication2.Infraestrutura;
 
 namespace WebApplication2.Areas.Seguranca.Controllers
@@ -25,5 +28,77 @@ namespace WebApplication2.Areas.Seguranca.Controllers
         {
             return View(GerenciadorUsuario.Users);
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (string error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Create(UsuarioViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Usuario user = new Usuario
+                {
+                    UserName = model.Nome,
+                    Email = model.Email
+                };
+                IdentityResult result = GerenciadorUsuario.Create(user, model.Senha);
+                if (result.Succeeded)
+                { return RedirectToAction("Index"); }
+
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+                return View(model);
+            }
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = GerenciadorUsuario.FindById(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            // inicia o objeto usuário para visão
+            var uvm = new UsuarioViewModel();
+            uvm.Id = usuario.Id;
+            uvm.Nome = usuario.UserName;
+            uvm.Email = usuario.Email;
+            return View(uvm);
+        }
+        [HttpPost]
+        public ActionResult Edit(UsuarioViewModel uvm)
+        {
+            if (ModelState.IsValid)
+            {
+                Usuario usuario = GerenciadorUsuario.FindById(uvm.Id);
+                usuario.UserName = uvm.Nome;
+                usuario.Email = uvm.Email;
+                usuario.PasswordHash = GerenciadorUsuario.PasswordHasher.
+                HashPassword(uvm.Senha);
+                IdentityResult result = GerenciadorUsuario.Update(usuario);
+                if (result.Succeeded)
+                { return RedirectToAction("Index"); }
+                else
+                { AddErrorsFromResult(result); }
+            }
+            return View(uvm);
+        }
     }
+
 }
